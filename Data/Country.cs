5b40df5d786bdea77283a3728d9d166738d0417d
Olga Shtykova <olgashtykova@yahoo.com>
2021-11-14 22:43:50 +0300

    Add DTOs.

--- Data/Country.cs
+++ Data/Country.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace HotelListing.Data
 {
@@ -10,5 +7,7 @@ namespace HotelListing.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
+
+        public virtual IList<Hotel> Hotels { get; set; }
     }
 }
