d4d7c47af67305c496f13906529cdf0ce87d99aa
Olga Shtykova <olgashtykova@yahoo.com>
2021-11-14 22:50:04 +0300

    Add AutoMapper.

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using HotelListing.Configurations;
 using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -39,6 +40,8 @@ namespace HotelListing
                 .AllowAnyMethod()
                 .AllowAnyHeader());
             });
+
+            services.AddAutoMapper(typeof(MapperInitializer));
 
             services.AddSwaggerGen(c =>
             {
