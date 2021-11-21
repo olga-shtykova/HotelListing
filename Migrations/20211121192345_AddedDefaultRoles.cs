013a463a17b4344308ef86527b8238abffcd9132
Olga Shtykova <olgashtykova@yahoo.com>
2021-11-21 22:30:12 +0300

    Update DatabaseContext.

---
+++ Migrations/20211121192345_AddedDefaultRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class AddedDefaultRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "63a24af8-4baf-44b2-aa62-1d3ad1c40db0", "727a3329-01f1-4820-b0b0-740d443e0ca6", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b2ebb26f-9921-4a94-ac56-29ab2744f2d2", "07d2921d-a241-45db-992a-75f1186fa3b0", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "63a24af8-4baf-44b2-aa62-1d3ad1c40db0");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b2ebb26f-9921-4a94-ac56-29ab2744f2d2");
+        }
+    }
+}
