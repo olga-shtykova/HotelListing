88c0b99c9f02e20b5e94c069a8d58653f40523c0
Olga Shtykova <olgashtykova@yahoo.com>
2021-11-21 22:06:53 +0300

    Add AccountController.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<ApiUser> userManager,
+            ILogger<AccountController> logger,
+            IMapper mapper)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            _logger.LogInformation($"Registration attempt for {userDTO.Email}");
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var user = _mapper.Map<ApiUser>(userDTO);
+                user.UserName = userDTO.Email;
+
+                var result = await _userManager.CreateAsync(user, userDTO.Password);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+
+                    return BadRequest(ModelState);
+                }
+
+                return Accepted();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(Register)}");
+
+                return Problem($"Something went wrong in the {nameof(Register)}", statusCode: 500);
+            }
+        }
+
+        //[HttpPost]
+        //[Route("login")]
+        //[ProducesResponseType(StatusCodes.Status202Accepted)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<ActionResult> Login([FromBody] LoginUserDTO loginApiUserDTO)
+        //{
+        //    _logger.LogInformation($"Login attempt for {loginApiUserDTO.Email}");
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    try
+        //    {
+        //        var result = await _signInManager.PasswordSignInAsync(loginApiUserDTO.Email,
+        //            loginApiUserDTO.Password, false, false);
+
+        //        if (!result.Succeeded)
+        //        {
+        //            return Unauthorized(loginApiUserDTO);
+        //        }
+
+        //        return Accepted();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, $"Something went wrong in the {nameof(Login)}");
+
+        //        return Problem($"Something went wrong in the {nameof(Login)}", statusCode: 500);
+        //    }
+        //}
+    }
+}
--- ServiceExtentions.cs
+++ ServiceExtentions.cs
@@ -1,10 +1,6 @@
 ﻿using HotelListing.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HotelListing
 {
